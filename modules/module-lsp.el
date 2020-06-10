a22e4174cc820521e5c0f6721019e1d8562120e7
rsauex <yuragrig@ukr.net>
2020-06-10 22:25:07 +0300

    Change LSP prefix key

--- modules/module-lsp.el
+++ modules/module-lsp.el
@@ -1,5 +1,7 @@
 (use-package lsp-mode :ensure t
   :commands (lsp lsp-mode)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
   :config
   (setq lsp-completion-show-detail nil
         lsp-session-file (expand-file-name "lsp-session-v1" my-cache-dir)))
