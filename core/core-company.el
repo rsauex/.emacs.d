92f1e01c8eb35ec0939bd879ca376a2f7e6e19a3
rsauex <rsauex@mailbox.org>
2022-07-12 19:58:03 +0300

    Rewrite company settings

--- core/core-company.el
+++ core/core-company.el
@@ -1,15 +1,22 @@
-(use-package company
-  :ensure t
-  :diminish company-mode
-  :hooks ((after-init-hook . (global-company-mode)))
-  :custom
-  (company-tooltip-align-annotations t))
 ;; -*- lexical-binding: t; -*-
 
 (use-package company-box
   :ensure t
-  :hooks ((company-mode-hook . (company-box-mode)))
+  :diminish company-box-mode
   :custom
   (company-box-enable-icon nil))
+
+(use-package company
+  :ensure t
+  :diminish company-mode
+  :hooks
+  ((after-init-hook . (global-company-mode))
+   (company-mode-hook . (company-box-mode)))
+  :bind
+  (;; Remap completion-at-point
+   :map company-mode-map
+   ("C-M-i" . company-complete))
+  :custom
+  (company-tooltip-align-annotations t))
 
 (provide 'core-company)
