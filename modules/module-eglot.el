bfd20eb67398b68cdeaceeda8f07f1161b058121
rsauex <rsauex@mailbox.org>
2026-02-21 08:22:28 +0200

    Don't start long-starting lsp servers separately for each file

--- modules/module-eglot.el
+++ modules/module-eglot.el
@@ -9,7 +9,7 @@
   (eglot-connect-timeout 600)
   ;; Don't store events, they just take up space unless we're debugging a server or eglot
   (eglot-events-buffer-size 0)
-  ;; Don't start a separate lsp server when visiting files not in the project
+  ;; Activate Eglot in cross-referenced non-project files
   (eglot-extend-to-xref t)
   ;; Bind keys similar to how lsp-mode does it
   :bind ((:map eglot-mode-map)
@@ -21,7 +21,40 @@
          ("C-c l g r" . xref-find-references)
          ("C-c l r o" . eglot-code-action-organize-imports)
          ("C-c l r r" . eglot-rename)
-         ("C-c l a a" . eglot-code-actions)))
+         ("C-c l a a" . eglot-code-actions))
+  :init
+  (defvar my-eglot--started-servers
+    (make-hash-table :test #'equal))
+
+  (define-advice eglot--connect
+      (:around (orig-fn &rest args) dont-start-the-same-server-multiple-times)
+    "Eglot doesn't register a server before it's fully initialized. Because
+of this, a new instances of the same server will be started for each
+opened file before at least of one of those instances finishes
+initializing.
+
+This advice prevents eglot from starting multiple instances of the same
+server by keeping track of all the servers (not just the fully
+initialized ones), and throwing an error on an attempt to start a
+duplicate."
+    (if (let ((existing-server (gethash args my-eglot--started-servers)))
+          (and existing-server (jsonrpc-running-p existing-server)))
+        (eglot--error "Server already running!")
+      (cl-labels ((remember-started-server (server)
+                    (remove-hook 'eglot-server-initialized-hook #'remember-started-server t)
+                    (puthash args server my-eglot--started-servers)))
+        (remhash args my-eglot--started-servers)
+        (unwind-protect
+            (progn
+              (add-hook 'eglot-server-initialized-hook #'remember-started-server 'append t)
+              (apply orig-fn args))
+          (ignore-errors
+            (remove-hook 'eglot-server-initialized-hook #'remember-started-server t))))))
+
+  (defun eglot-ensure-only-in-project ()
+    "Only start eglot if the file is a part of a project."
+    (when (project-current)
+      (eglot-ensure))))
 
 (use-package eglot-supplements
   :vc (eglot-supplements :url "https://codeberg.org/harald/eglot-supplements"))
